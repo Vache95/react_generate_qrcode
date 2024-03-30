758c58238193717cf01fc1deb84dfbb292c8d6c4
Vache95 <vachevirabyan1995@gmail.com>
2024-03-30 20:43:39 +0400

    add react-color pakage

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import InputFeild from './InputFeild'
+import InputColor from './InputColor'
 
 const InputForm = () => {
   return (
     <div className='col-span-2 p-6 grid gap-4'>
        <InputFeild/>
+       <InputColor/>
     </div>
   )
 }
