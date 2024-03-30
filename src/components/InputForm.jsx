fcc3bb54b9bccc1b5b8c50b3249171dd19b7e7bb
Vache95 <vachevirabyan1995@gmail.com>
2024-03-30 20:25:20 +0400

    add url input

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InputFeild from './InputFeild'
+
+const InputForm = () => {
+  return (
+    <div className='col-span-2 p-6 grid gap-4'>
+       <InputFeild/>
+    </div>
+  )
+}
+
+export default InputForm
